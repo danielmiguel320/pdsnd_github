1e72bc287886d76fd7d0b5d4b3a704058fccf384
Daniel Miguel <daniel.miguel320@gmail.com>
2018-11-17 16:33:50 -0500

    refactor: Corected the restart the program feature.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -199,7 +199,7 @@ def main():
         user_stats(df)
         print_raw_data(df)
         restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes' or restart.lower() != 'y':
+        if restart.lower() != 'yes' and restart.lower() != 'y':
             break
 
 
